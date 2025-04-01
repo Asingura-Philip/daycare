bb8d5b633d88f711393725300d231b65cf10eb6b
Asingura Philip <asinguraphilip@gmail.com>
2025-04-01 22:15:02 +0300

    running server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.get('/',(req,res)=>{
+    res.send('This is the server')
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server running on port ${process.env.PORT}`)
+})
